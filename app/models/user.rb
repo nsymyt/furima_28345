a1206834ddf7958de436e4eb4438300c022d6c77
nsymyt <otuyamayihsin.4@gmail.com>
2020-08-07 17:45:13 +0900

    サインアップ画面のエラーメッセージ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, format:{ with:/[＠]/ }
-  validates :password, presence: true, length: {minimum: 6 }, confirmation: true, format: { with:/\A[a-zA-Z0-9]+\z/}
+  validates :email, uniqueness: true, format:{ with:/[＠]/ }
+  validates :password, length: {minimum: 6 }, confirmation: true, format: { with:/\A[a-zA-Z0-9]+\z/}
 
   with_options presence: true  do
     validates :first_name, format:{ with: /\A[ぁ-んァ-ン一-龥]/}
