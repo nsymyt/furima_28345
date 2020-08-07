3c856e6ebcc8dbb95ce70810f743bd3ab368fe7e
nsymyt <otuyamayihsin.4@gmail.com>
2020-08-07 17:19:41 +0900

    userモデルのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format:{ with:/[＠]/ }
+  validates :paswword, presence: true, length: {minimum: 6 }, confirmation: true, format: { with:/\A[a-zA-Z0-9]+\z/}
+
+  with_options presence: true do
+    validates :first_name, format:{ with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :last_name,  format:{ with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name_furigana, format:{ with: /\A[ァ-ヶー－]+\z/}
+    validates :last_name_furigana, format:{ with: /\A[ァ-ヶー－]+\z/}
+    validates :birthday_id, 
+  end
 end
