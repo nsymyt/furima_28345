f9e116ce7e5f4aec9a4a5bd113e6979f3787eb5b
nsymyt <otuyamayihsin.4@gmail.com>
2020-08-10 14:35:46 +0900

    Userモデルの単体テストコード

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :first_name, :last_name, :first_name_furigana, :last_name_furigana, :birthday_id])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :first_name, :last_name, :first_name_furigana, :last_name_furigana, :birthday_id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :email, uniqueness: { case_sensitive: true }
-  validates :password, length: { minimum: 6}, confirmation: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  validates :email, uniqueness: { case_sensitive: true }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, length: { minimum: 6 }, confirmation: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 
   with_options presence: true  do
-    validates :first_name, format:{ with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :last_name,  format:{ with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :first_name_furigana, format:{ with: /\A[ァ-ヶー－]+\z/}
-    validates :last_name_furigana, format:{ with: /\A[ァ-ヶー－]+\z/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name_furigana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_name_furigana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday_id
   end
 end
