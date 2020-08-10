c9622d7fa017c0b4da4e4aa71c46721d6a7d4088
nsymyt <otuyamayihsin.4@gmail.com>
2020-08-10 15:36:00 +0900

    Userモデルのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_EMAIL_REGEX    = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  VALID_NAME_REGEX     = /\A[ぁ-んァ-ン一-龥]/
+  VALID_FURIGANA_REGEX = /\A[ァ-ヶー－]+\z/
+  
   validates :name, presence: true
-  validates :email, uniqueness: { case_sensitive: true }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, length: { minimum: 6 }, confirmation: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :email, uniqueness: { case_sensitive: true }, format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { minimum: 6 }, confirmation: true, format: { with: VALID_PASSWORD_REGEX }
 
   with_options presence: true  do
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name_furigana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :last_name_furigana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name, format: { with: VALID_NAME_REGEX }
+    validates :last_name,  format: { with: VALID_NAME_REGEX }
+    validates :first_name_furigana, format: { with: VALID_FURIGANA_REGEX }
+    validates :last_name_furigana, format: { with: VALID_FURIGANA_REGEX }
     validates :birthday_id
   end
 end
