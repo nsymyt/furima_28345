ad05be21dbe9b6e780a44d5738aad80bdc9b3364
nsymyt <otuyamayihsin.4@gmail.com>
2020-08-10 11:09:30 +0900

    userテーブルのバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: { case_sensitive: true }
   validates :password, length: { minimum: 6}, confirmation: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
 
   with_options presence: true  do
