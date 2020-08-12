ff11b04d0b26d5d305b0c09261b62d7765fb942a
nsymyt <otuyamayihsin.4@gmail.com>
2020-08-12 17:03:16 +0900

    単体テストのFactoryBotの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :item do
-    image { Faker::Lorem.sentence }
-    name { Faker::Lorem.word }
-    text { Faker::Lorem.sentence }
-    category_id { Faker::Number.between(from: 2, to: 11) }
-    status_id { Faker::Number.between(from: 2, to: 7) }
-    delivery_fee_id { Faker::Number.between(from: 2, to: 3) }
-    prefecture_id { Faker::Number.between(from: 2, to: 48) }
-    days_until_shipping_id { Faker::Number.between(from: 2, to: 4) }
-    price { Faker::Number.between(from: 300, to: 9_999_999) }
+    image { "item-image" }
+    name { "アイウエオ" }
+    text { "カキクケコ"}
+    category_id {"2"}
+    status_id { "2" }
+    delivery_fee_id {"2" }
+    prefecture_id { "2"}
+    days_until_shipping_id { "2" }
+    price { "3000" }
 
     association :user
   end
